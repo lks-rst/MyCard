5bfe317d9160200d740445d62d8f33b2a64b099d
lks-rst <lks_rst@hotmail.com>
2022-01-11 19:04:09 -0300

    Create the vary base of the project. Puts out the layout with some widgets but it is all for now.

--- gsrwellcomecard/lib/main.dart
+++ gsrwellcomecard/lib/main.dart
@@ -2,11 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyCard()
   );
+}
+
+class MyCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return
+      MaterialApp(
+        home: Scaffold(
+          body: SafeArea(child: Center(
+            child: Column(
+              //crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(radius: 50, backgroundColor: Color.fromRGBO(
+                    161, 255, 255, 0.25),),
+                Text('GSR Sistemas', style: TextStyle(fontSize: 14, color: Colors.black, fontWeight: FontWeight.bold),),
+                Text('Out Sourcing', style: TextStyle(fontSize: 14, color: Colors.black, fontWeight: FontWeight.bold),),
+                Card(child: Text('Fone', style: TextStyle(fontSize: 14, color: Colors.black, fontWeight: FontWeight.bold),),),
+                Card(child: Text('Email', style: TextStyle(fontSize: 14, color: Colors.black, fontWeight: FontWeight.bold),),),
+              ],
+            ),
+          )),
+        ),
+      );
+  }
 }
